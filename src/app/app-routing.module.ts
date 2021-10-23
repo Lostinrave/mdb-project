ff7d3f05c8ce7cb82bbc8514ba86198e7ab52986
Lostinrave <83598946+Lostinrave@users.noreply.github.com>
2021-10-23 13:30:48 +0300

    Added components and child component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+// import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
+  // { path: '', component: HeaderComponent },
   { path: '', component: ProductsComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,21 @@ import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './products/products.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    NavBarComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
